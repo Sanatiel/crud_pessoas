b92c44499d101a7864be19906dc5c8aff7264151
Sanatiel Barros <sanatielbarros@gmail.com>
2016-05-20 16:35:30 -0300

    Layout do formulario de listagem completo

--- view/forms/form_list_people.php
+++ view/forms/form_list_people.php
@@ -16,10 +16,37 @@ $list_pes = $man->select("pessoas",null,null);
 
 <h3>Listagem de pessoas</h3>
 <br>
-<br>
+
 <form action="" method="POST">
+	<label>Tipo de busca:</label>
+	<br>
+	<input type="radio" name="campo" value="id"> Id 	&nbsp;
+	<input type="radio" name="campo" value="nome"> Nome 	&nbsp;
+	<input type="radio" name="campo" value="email"> Email &nbsp;
+	
+	<br>
+	<br>
+
+	<label>Ordenar por:</label>
+	<br>
+	<input type="radio" name="ordenacao" value="id"> Id 	&nbsp;
+	<input type="radio" name="ordenacao" value="nome"> Nome 	&nbsp;
+	<input type="radio" name="ordenacao" value="email"> Email &nbsp;
+	
+	<br>
+	<br>
+	<input type="text" name="busca" placeholder="Pesquisar">
+	<button class="btn btn-success">Pesquisar</button>
+
+
+	<!-- esse input vai mandar o id do cliente/servico/func a ser excluido -->
+	<input type="text" hidden value="" name="exclu" id="ex">
+
+	<br>
+	<br>
+
 	<table class="table table-hover">
-		
+
 	<tr>
 		<th class="text-center"> Id </th>
   		<th class="text-center"> Nome </th>
@@ -38,7 +65,7 @@ $list_pes = $man->select("pessoas",null,null);
 				echo '<td>',$key['nome_pes'],'</td>';
 				echo '<td>',$key['email_pes'],'</td>';
 				echo '<td>',$key['tel_pes'],'</td>';
-				echo '<td></td>';
+				echo '<td><a href="" class="btn btn-md btn-primary"><span class="glyphicon glyphicon-pencil"></span></a> <button class="btn btn-md btn-danger"><span class="glyphicon glyphicon-trash"></span></button>  </td>';
 			//	echo '<td><a href="'.base_url().'/view/getImg.php?PicNum=',$key['cod_pes'],'">vergoto</a></td>';
 			//	echo '<td><img src="',base_url(),'/view/getImg.php?PicNum=2"></td>';
 				echo '</tr>';
